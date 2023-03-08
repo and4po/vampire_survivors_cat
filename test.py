bbfd150e158036a21867a2b114c365933f1ad493
and4po <94716615+and4po@users.noreply.github.com>
2023-03-08 17:30:31 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-# Treure una cadena des d'uns separadors
-def substring(caracter_in, caracter_out, text):
-    index_in = text.index(caracter_in)
-    index_out = text.index(caracter_out) 
-    subcadena = text[index_in+1:index_out]
-    return subcadena
